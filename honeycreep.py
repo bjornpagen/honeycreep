ae8bfbbbf50fa6c1702d163208c64525d336b0f8
Bjorn Pagen <bjornpagen@gmail.com>
2017-05-27 16:36:02 -0400

    oaesnuth

--- honeycreep.py
+++ honeycreep.py
@@ -1,7 +1,6 @@
 #!/bin/env python3
 
-import sys, os
-import getpass
+import sys, os, argparse, textwrap
 from getters import *
 
 
@@ -17,15 +16,15 @@ def main():
         "The only way to deal with an unfree world is to become so absolutely 
         free that your very existence is an act of rebellion." - Albert Camus
 
-            created by bjornpagen@gmail.com
+            -created by bjornpagenatgmaildotcom
         ''')
     )
 
-    parser.add_argument("-t", "--title", metavar="NAME_OF_BOOK", type="str", help="title of the desired book")
-    parser.add_argument("-e", "--engine", metavar="NAME_OF_ENGINE", type="str", help="method of downloading books")
-    parser.add_argument("-c'", "--cse-id", metavar="CSE_ID", type="str", help="custom cse id for google")
-    parser.add_argument("-a", "--api-key", metavar="API_KEY", type="str", help="custom api key for google")
-    parser.add_argument("-d", "--dir", metavar="DIR", type="str", help="", default = os.getcwd())
+    parser.add_argument("t", metavar="NAME_OF_BOOK", type=str, help="title of the desired book")
+    parser.add_argument("-e", "--engine", metavar="NAME_OF_ENGINE", type=str, help="method of downloading books")
+    parser.add_argument("-c", "--cse-id", metavar="CSE_ID", type=str, help="custom cse id for google")
+    parser.add_argument("-a", "--api-key", metavar="API_KEY", type=str, help="custom api key for google")
+    parser.add_argument("-d", "--dir", metavar="DIR", type=str, help="dir to download books", default = os.getcwd())
 
     args = parser.parse_args()
 
